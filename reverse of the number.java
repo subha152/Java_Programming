56b4082c4d945a2ad2e349dae5fa99176b410725
subha152 <51635202+subha152@users.noreply.github.com>
2021-08-13 12:46:55 +0530

    Add files via upload

---
+++ reverse of the number.java
@@ -0,0 +1,18 @@
+
+package oops;
+import java.util.Scanner;
+public class B {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int num,temp,r,sum=0;
+        System.out.println("Enter Number: ");
+        num=input.nextInt();
+        temp=num;
+        while(temp!=0){
+            r=temp%10;
+            sum=sum*10+r;
+            temp=temp/10;
+        }
+        System.out.println("Sum of the digit: "+sum);
+    }
+}
