eb04e49c20adcf706ead25c5f1d50a9dd087a1bc
Subha Ghosh <51635202+subha152@users.noreply.github.com>
2021-11-11 11:39:46 +0530

    Add files via upload

---
+++ copyConstract.java
@@ -0,0 +1,24 @@
+package exam;
+public class CopyConstractor {
+    int id;
+    String name;
+    CopyConstractor(int i,String n){
+        id=i;
+        name=n;
+    }
+    CopyConstractor(CopyConstractor cp){
+        id=cp.id;
+        name=cp.name;
+    }
+    void display(){
+        System.out.println("Id: "+id);
+        System.out.println("Name: "+name);
+    }
+    public static void main(String[] args) {
+        CopyConstractor obj=new CopyConstractor(1,"Subha");
+        CopyConstractor obj1=new CopyConstractor(obj);
+        obj.display();
+        obj1.display();
+    }
+}
+subha
